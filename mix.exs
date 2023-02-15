4614861f557a48e5e06df5e025e7c7a6e1b77073
Helder Sousa <helderhenri@gmail.com>
2023-02-15 05:38:56 -0300

    chore: publish on hex

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Value.MixProject do
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
       preferred_cli_env: [
         "test.watch": :test
       ]
@@ -21,11 +23,27 @@ defmodule Value.MixProject do
     ]
   end
 
+  defp description() do
+    "Get value from struct or map with json path"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "value",
+      # These are the default files included in the package
+      files: ~w(lib test .formatter.exs mix.exs README*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/team-softaliza/value"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:flow, "~> 1.0"},
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
